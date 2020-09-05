076590882d871c457c82c03da73721ec3f848fcd
FalconA91Khalil <66460614+FalconA91Khalil@users.noreply.github.com>
2020-09-05 23:35:23 +0500

    Update Delete

--- RegisterRhUI/wwwroot/js/App/form.js
+++ RegisterRhUI/wwwroot/js/App/form.js
@@ -42,26 +42,29 @@ function loadDataTable() {
 }
 function Delete(url) {
     swal({
-        title: "Are you sure you want to delete?",
-        text: "You will not be able to restore the content!",
-        type: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#DD6B55",
-        confirmButtonText: "Yes, delete it!",
-        closeOnconfirm: true
-    }, function () {
-        $.ajax({
-            type: 'DELETE',
-            url: url,
-            success: function (data) {
-                if (data.success) {
-                    toastr.success(data.message);
-                    dataTable.ajax.reload();
-                }
-                else {
-                    toastr.error(data.message);
-                }
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this imaginary file!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+    })
+        .then((willDelete) => {
+            if (willDelete) {
+                $.ajax({
+                    type: 'DELETE',
+                    url: url,
+                    success: function (data) {
+                        swal("Poof! Your imaginary file has been deleted!", {
+                            icon: "success",
+                        });
+                        dataTable.ajax.reload();
+                    },
+
+                });
+                
+            } else {
+                swal("Your imaginary file is safe!");
             }
         });
-    });
 }
+
