a162ff7cf45565ac3dd1a9663c6add2cb6d3d09b
FalconA91Khalil <66460614+FalconA91Khalil@users.noreply.github.com>
2020-09-05 22:47:25 +0500

    Update Form

---
+++ Common/Utility/SharedBag.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Utility
+{
+    public class SharedBag
+    {
+        public const string JqueryDateFormat = "";
+    }
+}
